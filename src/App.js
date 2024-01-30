c8091fb12f8555f4c62f5134ac78235cc18e12b2
KRANTHI-REDDY\pc <kranthi.sama22@gmail.com>
2024-01-30 21:48:39 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hey Man Welcome to Amplify...
         </a>
       </header>
     </div>
